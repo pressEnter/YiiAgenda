d853dbc11c3cac6960386c141d9444b1f88f45e8
Luciano Baraglia <luciano.baraglia@gmail.com>
2013-07-28 02:25:33 -0300

    DetailView

--- views/site/details.php
+++ views/site/details.php
@@ -1,27 +1,23 @@
 <?php
+use \yii\widgets\DetailView;
 /**
  * @var yii\base\View $this
  */
 $this->title = 'Agenda - Card details';
 ?>
 <h3>Card details</h3>
-<table class="table table-bordered table-hover">
-	<tbody>
-		<tr>
-			<th>ID</th>
-			<td><?php echo $card->id; ?></td>
-		</tr>
-		<tr>
-			<th>Title</th>
-			<td><?php echo $card->title; ?></td>
-		</tr>
-		<tr>
-			<th>Name</th>
-			<td><?php echo $card->name; ?></td>
-		</tr>
-		<tr>
-			<th>Category</th>
-			<td><?php echo $card->category->name . " | " . $card->category->color; ?></td>
-		</tr>
-	</tbody>
-</table>
+<?php 
+echo DetailView::widget(array(
+	'model' => $card,
+	'attributes' => array(
+		'id',
+		'title',
+		'name',
+		array(
+			'label' => 'Category',
+			'value' => $card->category->name . " | " . $card->category->color,
+		),
+	),
+));
+?>
+ 
